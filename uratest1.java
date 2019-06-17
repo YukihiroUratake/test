15c8358b7b5262bc90ebb62765bfe95f9dcac454
Yukihiro Uratake <51037352+YukihiroUratake@users.noreply.github.com>
2019-06-17 11:26:47 +0900

    Add files via upload
    元に戻す

--- uratest1.java
+++ uratest1.java
@@ -1,6 +1,7 @@
+<<<<<<< Updated upstream:auratest1.java
 //nに入れた数字が10以上ならtrue、それ以外ならfalseを返すプログラム
 
-//0617 GITHUB修正
+//0617修正
 
 class uratest1{
     public static void main(String[] args){
@@ -17,3 +18,24 @@ class uratest1{
         }
     }
 }
+=======
+//nに入れた数字が10以上ならtrue、それ以外ならfalseを返すプログラム
+
+//0617修正
+
+class uratest1{
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(   judge(n)   );
+    }
+
+    public static boolean judge(   int    x){
+        if(x>=10){
+            return    true   ;
+        }
+        else{
+            return    false  ;
+        }
+    }
+}
+>>>>>>> Stashed changes:uratest1.java
