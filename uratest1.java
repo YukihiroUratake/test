3bcb3cf14e530249b00f6eebf4da4f4413abe2ba
yukihiro_uratake <mgb2han@yahoo.co.jp>
2019-06-17 11:17:58 +0900

    名前を元に戻す

---
+++ uratest1.java
@@ -0,0 +1,41 @@
+<<<<<<< Updated upstream:auratest1.java
+//nに入れた数字が10以上ならtrue、それ以外ならfalseを返すプログラム
+
+//0617修正
+
+class uratest1{
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(   judge(n)   );
+    }
+
+    public static boolean judge(   int    x){
+        if(x>=10){
+            return    true   ;
+        }
+        else{
+            return    false  ;
+        }
+    }
+}
+=======
+//nに入れた数字が10以上ならtrue、それ以外ならfalseを返すプログラム
+
+//0617修正
+
+class uratest1{
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(   judge(n)   );
+    }
+
+    public static boolean judge(   int    x){
+        if(x>=10){
+            return    true   ;
+        }
+        else{
+            return    false  ;
+        }
+    }
+}
+>>>>>>> Stashed changes:uratest1.java
